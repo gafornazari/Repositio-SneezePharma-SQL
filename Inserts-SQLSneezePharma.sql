e3cfb309f9cbbbe61caebeeaec498002f0e77719
gafornazari <166765629+gafornazari@users.noreply.github.com>
2025-11-15 11:20:35 -0300

    Refactor SQL inserts and updates for clarity

--- Inserts-SQLSneezePharma.sql
+++ Inserts-SQLSneezePharma.sql
@@ -1,73 +1,12 @@
---inserindo fornecedores
-INSERT INTO Fornecedores (RazaoSocial, Pais, DataAbertura, DataCadastro, CNPJ, Situacao)
-VALUES
-	('5BY5 - CONSULTORIA EM TI LTDA', 'Brasil', '2008-03-20', GETDATE(), '16598745635412', 'A'),
-	('SHOPPING JARAGUA', 'Brasil', '2011-09-09', GETDATE(), '12365874568451', 'A'),
-	('VENDINHA SEU Z…', 'Brasil', '2022-09-18', GETDATE(), '12345678910234', 'A'),
-	('MERCADO TEM TUDO', 'Brasil', '2019-02-08', GETDATE(), '98745632102365', 'I'),
-	('SHOPPING LUPO', 'Brasil', '2018-10-31', GETDATE(), '65874697548965', 'A'),
-	('EXTRA', 'Brasil', '2022-08-12', GETDATE(), '03258749658741', 'A');
-
---alterando a situaÁ„o do fornecedor
-UPDATE Fornecedores
-SET Situacao = 'A'
-WHERE idFornecedor = 4;
-SELECT * FROM Fornecedores;
-
-
---inserindo fornecedores bloqueados
-INSERT INTO FornecedoresBloqueados(idFornecedor)
-VALUES
-	(4), (6);
-SELECT * FROM FornecedoresBloqueados;
-
---inserindo principios ativos
-INSERT INTO PrincipiosAtivos(DataCadastro, Nome, Situacao)
-VALUES
-	(GETDATE(), 'Citrato de Orfenadrina', 'A'),
-	(GETDATE(), 'Dipirona Monoidratada', 'A'),
-	(GETDATE(), '¡cido AscÛrbico', 'A'),
-	(GETDATE(), 'Colecalciferol', 'A');
-SELECT * FROM PrincipiosAtivos;
-
---inserindo compras
-INSERT INTO Compras(Datacompra, idFornecedor)
-VALUES
-	(GETDATE(), 2),
-	(GETDATE(), 3),
-	(GETDATE(), 3),
-	(GETDATE(), 5),
-	(GETDATE(), 5);
-
---inserindo itens compras
-INSERT INTO ItensCompras (idPrincipioAtivo, Quantidade, ValorUnitario)
-VALUES (1, 10, 5.00),
-       (2, 20, 10.00),
-       (3, 30, 15.00),
-       (4, 40, 20.00);
-SELECT * FROM ItensCompras;
-
---inserindo compra itens compra
-INSERT INTO Compras_ItensCompra (idCompra, idItemCompra)
-VALUES
-	(1, 1),
-    (1, 2),
-    (1, 3);
-SELECT * FROM Compras
-SELECT * FROM ItensCompras
-INSERT INTO Compras_ItensCompra(idCompra, idItemCompra)
-VALUES
-	(2, 2);
-SELECT * FROM Compras_ItensCompra;
-
 --inserindo clientes
-INSERT INTO Clientes (Nome, CPF, DataCadastro, DataNascimento, Situacao)
-VALUES
-	('Giovanna Fornazari', '42443294803', GETDATE(), '2006/05/24', 'A'),
-	('Joao Silva', '12345678934', GETDATE(), '2000/10/21', 'A'),
-	('Carol Dias', '78978978978', GETDATE(), '1987/02/08', 'A'),
-	('Kaio Bandeira', '12363254125', GETDATE(), '1950/12/25', 'A')
-	
+EXEC sp_Clientes @Nome='Giovanna Fornazari', @CPF='42443294803', @DataNascimento = '2006/05/24', @Contato = '16999999999';
+EXEC sp_Clientes @Nome =N'Joao Silva', @CPF =  '12345678934', @DataNascimento ='2000/10/21', @Contato = N'14963259696';
+EXEC sp_Clientes @Nome =N'Carol Dias', @CPF =  '78978978978', @DataNascimento ='1987/02/08', @Contato = N'11987458585';
+EXEC sp_Clientes @Nome =N'Kaio Bandeira', @CPF =  '12363254125', @DataNascimento ='1950/12/25', @Contato = N'16996525555';
+
+SELECT * FROM Clientes
+SELECT * FROM TelefonesClientes
+
 --alterando situacao do cliente
 UPDATE Clientes
 SET Situacao = 'I'
@@ -78,20 +17,17 @@ SELECT * FROM Clientes
 DELETE FROM Clientes
 WHERE idCliente = 1
 
---inserindo telefone para alguns clientes
-INSERT INTO TelefonesClientes (Contato, idCliente)
-VALUES
-	('16999999999',1),
-	('14963259696',1),
-	('11987458585',2),
-	('16996525555',3);
-SELECT * FROM TelefonesClientes
-
 --inserindo clientes restritos
 INSERT INTO ClientesRestritos(idCliente)
 VALUES
 	(2);
 SELECT * FROM ClientesRestritos
+
+--inserindo outro telefone para o cliente de id 2
+INSERT INTO TelefonesClientes (Contato, idCliente)
+VALUES
+	('16999995999',2)
+
 
 --inserindo medicamentos
 INSERT INTO Medicamentos (CDB, Nome, Categoria, ValorVenda, DataCadastro, Situacao)
@@ -101,58 +37,101 @@ VALUES
 	('7894561230001', 'Vitamina C', 'V',30.99 , GETDATE(), 'A'),
 	('7895552226523', 'Vitamina C Power', 'V',80.99 , GETDATE(), 'A')
 	
---alterando a situaÁ„o dos medicamentos
+--alterando a situa√ß√£o dos medicamentos
 UPDATE Medicamentos
 SET Situacao = 'I'
 WHERE idMedicamento IN (1,3)
 SELECT * FROM Medicamentos
 
--- inserindo itens de produÁ„o
-INSERT INTO ItensProducao (idPrincipioAtivo, QuantidadePA)
-VALUES
-	(1, 2.5),
-	(2, 5.0),
-	(3, 1.0),
-	(4, 0.5);
-SELECT * FROM ItensProducao;
+--inserindo uma venda
+EXEC sp_VendasMedicamentos @idCliente = 1 , @idMedicamento = 2, @Quantidade = 5;
+EXEC sp_VendasMedicamentos @idCliente = 3 , @idMedicamento = 3, @Quantidade = 10;
+EXEC sp_VendasMedicamentos @idCliente = 1 , @idMedicamento = 2, @Quantidade = 2;
 
--- inserindoproduÁıes
-INSERT INTO Producoes (idMedicamento, DataProducao, Quantidade)
+SELECT * FROM VendasMedicamentos
+SELECT * FROM Vendas_ItensVenda
+SELECT * FROM ItensVenda
+
+
+--inserindo fornecedores
+EXEC sp_Fornecedores @RazaoSocial='5BY5 - CONSULTORIA EM TI LTDA', @CNPJ='16598745635412', @Pais='Brasil', @DataAbertura='2008-03-20';
+EXEC sp_Fornecedores @RazaoSocial='SHOPPING JARAGUA', @CNPJ='12365874568451', @Pais='Brasil', @DataAbertura='2011-09-09';
+EXEC sp_Fornecedores @RazaoSocial='VENDINHA SEU Z√â', @CNPJ='12345678910234', @Pais='Brasil', @DataAbertura='2022-09-18';
+EXEC sp_Fornecedores @RazaoSocial='MERCADO TEM TUDO', @CNPJ='98745632102365', @Pais='Brasil', @DataAbertura='2019-02-08';
+EXEC sp_Fornecedores @RazaoSocial='SHOPPING LUPO', @CNPJ='65874697548965', @Pais='Brasil', @DataAbertura='2018-10-31';
+EXEC sp_Fornecedores @RazaoSocial='EXTRA', @CNPJ='03258749658741', @Pais='Brasil', @DataAbertura='2022-08-12';
+
+SELECT * FROM Fornecedores
+
+--alterando a situa√ß√£o do fornecedor
+UPDATE Fornecedores
+SET Situacao = 'I'
+WHERE idFornecedor = 3;
+SELECT * FROM Fornecedores;
+
+--testando se o delete est√° sendo bloqueado
+DELETE FROM Fornecedores
+WHERE idFornecedor = 1;
+
+--inserindo fornecedores bloqueados
+INSERT INTO FornecedoresBloqueados(idFornecedor)
+VALUES (2);
+SELECT * FROM FornecedoresBloqueados;
+
+--inserindo princ√≠pios ativos
+INSERT INTO PrincipiosAtivos(DataCadastro, Nome, Situacao)
 VALUES
-	(1, '2025-10-01', 1000),
-	(2, '2025-10-02', 800),
-	(3, '2025-10-03', 600),
-	(4, '2025-10-04', 400);
+	(GETDATE(), 'Citrato de Orfenadrina', 'A'),
+	(GETDATE(), 'Dipirona Monoidratada', 'A'),
+	(GETDATE(), '√Åcido Asc√≥rbico', 'A'),
+	(GETDATE(), 'Colecalciferol', 'A');
+
+--alterando situa√ß√£o de alguns princ√≠pios ativos
+UPDATE PrincipiosAtivos
+SET Situacao = 'I'
+WHERE idPrincipioAtivo IN (1,3);
+SELECT * FROM PrincipiosAtivos;
+
+--inserindo produ√ß√£o
+EXEC sp_Producoes
+    @idPrincipioAtivo = 2,
+    @idMedicamento = 4,
+    @QuantidadeProducao = 20,
+    @QuantidadePA = 20;
+
+EXEC sp_Producoes
+    @idPrincipioAtivo = 4,
+    @idMedicamento = 2,
+    @QuantidadeProducao = 27,
+    @QuantidadePA = 27;
+
 SELECT * FROM Producoes;
-
--- producoes_itensProducao
-INSERT INTO Producoes_ItensProducao (idProducao, idItemProducao)
-VALUES
-	(1, 1), (1, 2), (1, 2);
-INSERT INTO Producoes_ItensProducao (idProducao, idItemProducao)
-VALUES
-	(1, 2), (2, 3), (3, 2);
+SELECT * FROM ItensProducao;
 SELECT * FROM Producoes_ItensProducao;
 
---inserindo venda
-INSERT INTO VendasMedicamentos (idCliente, DataVenda)
-VALUES
-	(1, GETDATE());
-SELECT * FROM VendasMedicamentos;
+--inserindo compras
+EXEC sp_Compras @idPrincipioAtivo = 2, @Quantidade = 100, @ValorUnitario = 5.50, @idFornecedor = 1;
+EXEC sp_Compras @idPrincipioAtivo = 4, @Quantidade = 50, @ValorUnitario = 8.00, @idFornecedor = 5;
+SELECT * FROM Compras;
+SELECT * FROM ItensCompras;
+SELECT * FROM Compras_ItensCompra;
 
---inserindo itens venda
-INSERT INTO ItensVenda (idMedicamento, Quantidade)
-VALUES
-	(1, 2),
-	(2, 1),
-	(3, 3),
-	(4, 1);
-SELECT * FROM ItensVenda;
 
---inserindo vendas_intensVenda
-INSERT INTO Vendas_ItensVenda (idVenda, idItemVenda)
-VALUES
-	(1, 1),
-	(1, 2),
-	(1, 3);
-SELECT * FROM Vendas_ItensVenda;
+SELECT * FROM Clientes
+SELECT * FROM ClientesRestritos
+SELECT * FROM TelefonesClientes
+SELECT * FROM VendasMedicamentos
+SELECT * FROM ItensVenda
+SELECT * FROM Vendas_ItensVenda
+SELECT * FROM Medicamentos
+SELECT * FROM Producoes
+SELECT * FROM ItensProducao
+SELECT * FROM Producoes_ItensProducao
+SELECT * FROM PrincipiosAtivos
+SELECT * FROM Compras
+SELECT * FROM ItensCompras
+SELECT * FROM Compras_ItensCompra
+SELECT * FROM Fornecedores
+SELECT * FROM FornecedoresBloqueados
+
+
